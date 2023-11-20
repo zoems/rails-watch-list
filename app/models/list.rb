39ceaed5b1ff9fcb2c4bb8b11a2c9977ca884618
Zoe Mawby Smart <zoemsmart@gmail.com>
2023-11-20 10:27:42 +0100

    crud funtion

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, uniqueness: true, presence: true
+  has_one_attached :photo
 end
